e233e98934c4191318975e2963f980bab2833380
Andreas Hubel <andi@saerdnaer.de>
2023-09-30 15:00:54 +0200

    upgrade dependencies

--- osm-api-gw/src/main.ts
+++ osm-api-gw/src/main.ts
@@ -1,6 +1,6 @@
-import { createServer } from '@graphql-yoga/node'
+import { createServer } from 'http'
+import { createYoga } from 'graphql-yoga'
 import { useGraphQLModules } from '@envelop/graphql-modules'
-
 
 import config from './config';
 import { application } from './application'
@@ -8,14 +8,19 @@ import { buildSchema } from './schema';
 
 const main = async () => {
 
-  const server = createServer({
-    port: config.port as number,
+  const yoga = createYoga({
     schema: await buildSchema(),
     plugins: [useGraphQLModules(application)],
-  })
+    graphiql: {
+      title: 'OSM GraphiQL',
+      showAttribution: false,
+    } as any,
+  });
 
-  await server.start();
-  console.log(`🚀 osm-api-gw is running on http://localhost:${config.port}/graphql`);
+  const server = createServer(yoga);
+  server.listen(config.port, () => {
+    console.log(`🚀 osm-api-gw is running on http://localhost:${config.port}/graphql`);
+  })
 }
 
 main();
