8766ab85fa677946cefa62547ddc6b38d75a7779
Andreas Hubel <andi@saerdnaer.de>
2023-10-01 13:40:35 +0200

    chore: host graphiql locally + add example query

--- osm-api-gw/src/main.ts
+++ osm-api-gw/src/main.ts
@@ -1,19 +1,27 @@
 import { createServer } from 'http'
 import { createYoga } from 'graphql-yoga'
+import { renderGraphiQL } from '@graphql-yoga/render-graphiql'
 import { useGraphQLModules } from '@envelop/graphql-modules'
 
 import config from './config';
 import { application } from './application'
 import { buildSchema } from './schema';
+import { loadQuery } from './utils';
+
 
 const main = async () => {
 
   const yoga = createYoga({
     schema: await buildSchema(),
     plugins: [useGraphQLModules(application)],
+    landingPage: false,
+    maskedErrors: false,
+    renderGraphiQL,
+    graphqlEndpoint: {} as any, // we want to answer to both / and /graphql
     graphiql: {
       title: 'OSM GraphiQL',
       showAttribution: false,
+      defaultQuery: loadQuery('examples/default'),
     } as any,
   });
 
